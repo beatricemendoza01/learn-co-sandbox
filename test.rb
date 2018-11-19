bf91173d86cc53b33f9ce619189271f06df6404e
Beatrice Mendoza <mendoza@nyu.edu>
2018-11-19 15:01:29 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,11 @@
+contacts = {
+  jon_snow: {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]  },
+  freddy_mercury: {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
